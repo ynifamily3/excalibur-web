01a80d5d5499bb6a76dc182568e90b07f5823add
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-10-21 19:00:30 +0900

    feat : 컨텐츠 업데이트

--- src/components/pages/Intro.tsx
+++ src/components/pages/Intro.tsx
@@ -15,6 +15,10 @@ const Intro = (): JSX.Element => {
       <Gnb />
       <Article>
         <P>안녕하세요, Excalibur입니다.</P>
+        <P>인트로를 새로 했습니다. &gt;ㅁ&lt;</P>
+        <P>
+          Excalibur는 SwordMaster에서 개발한, 화상강의 집중력 향상 솔루션입니다.
+        </P>
       </Article>
       <Footer />
     </>
